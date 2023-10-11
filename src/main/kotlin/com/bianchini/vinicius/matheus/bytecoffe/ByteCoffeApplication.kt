8aa31d308a84284e4f8247a292882fb26f2ebfc5
Matheus Vinicius Bianchini <mvbbianchini@hotmail.com>
2023-10-11 20:07:57 -0300

    Initial commit

---
+++ src/main/kotlin/com/bianchini/vinicius/matheus/bytecoffe/ByteCoffeApplication.kt
@@ -0,0 +1,12 @@
+package com.bianchini.vinicius.matheus.bytecoffe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.runApplication
+
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
+class ByteCoffeApplication
+
+fun main(args: Array<String>) {
+    runApplication<ByteCoffeApplication>(*args)
+}
---
+++ src/main/kotlin/com/bianchini/vinicius/matheus/bytecoffe/controllers/ProductController.kt
@@ -0,0 +1,17 @@
+package com.bianchini.vinicius.matheus.bytecoffe.controllers
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/products")
+class ProductController {
+
+    // TODO: retornar lista de produtos
+    @GetMapping
+    fun getAllProduct(): ResponseEntity<Any> {
+        return ResponseEntity.ok("deu boa!")
+    }
+}
